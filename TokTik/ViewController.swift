5fd598960048c155c11c8778d58af689e104269a
daniB <daniel.bolivar@glancepay.com>
2019-02-21 22:16:32 -0800

    Initial Commit

---
+++ TokTik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TokTik
+//
+//  Created by Daniel Bolivar Herrera on 21/02/2019.
+//  Copyright © 2019 Daniel Bolivar Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

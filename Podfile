b2225190bce7efcb55e74d3b8b914a104c820338
daniB <daniel.bolivar@glancepay.com>
2019-02-26 20:43:13 -0800

    Added SVProgressHud

--- Podfile
+++ Podfile
@@ -6,6 +6,7 @@ target 'TokTik' do
   # Pods for TokTik
   pod 'Alamofire', '~> 5.0.0-beta.2'
   pod 'SwiftMessages', '~> 6.0.2'
+  pod 'SVProgressHUD'
 
   target 'TokTikTests' do
     inherit! :search_paths
---
+++ TokTik/Presentation/Utilities + Protocol/UIActivityHUD.swift
@@ -0,0 +1,30 @@
+//
+//  UIActivityHUD.swift
+//  TokTik
+//
+//  Created by Daniel Bolivar Herrera on 26/02/2019.
+//  Copyright © 2019 Daniel Bolivar Herrera. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+
+protocol UIActivityHUD: class {
+    func showHUD(status: String?) // Presents an activity view indicating an loading process
+    func hideHUD()
+}
+
+// MARK :- GPMessageViewProtocol default implementation for UIViewController subclasses
+
+extension GPUIActivityHUD where Self: UIViewController {
+    
+    func showHUD(status: String?=nil) {
+        SVProgressHUD.setContainerView(self.view)
+        SVProgressHUD.show(withStatus: status)
+    }
+    
+    func hideHUD() {
+        SVProgressHUD.dismiss()
+    }
+}

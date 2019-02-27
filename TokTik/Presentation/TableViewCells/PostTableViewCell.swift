09e735165c9ada457893e9c6b9f93b8786f4729a
daniB <daniel.bolivar@glancepay.com>
2019-02-26 20:40:13 -0800

    UI Checkpoint - Video reproducing and scrolling

--- TokTik/Presentation/TableViewCells/PostTableViewCell.swift
+++ TokTik/Presentation/TableViewCells/PostTableViewCell.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PostTableViewCell: UITableViewCell {
 
+    @IBOutlet private weak var videoTitle: UILabel!
+    
+        @IBOutlet private weak var videoContainer: UIView!
+    
+    private var player: AVPlayer!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,4 +28,29 @@ class PostTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    func setup(title: String, videoUrl: URL) {
+        self.videoTitle.text = title
+        
+        let videoUrl = URL(string: "https://stream.livestreamfails.com/video/5c6da33e290b8.mp4")!
+        //Play video
+        let videoAsset = AVAsset(url: videoUrl)
+        // Create an AVPlayerItem with asset
+        let videoPlayerItem = AVPlayerItem(asset: videoAsset)
+        
+        // Initialize player with the AVPlayerItem instance.
+        self.player = AVPlayer(playerItem: videoPlayerItem)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.videoGravity = .resizeAspectFill
+        playerLayer.frame = self.bounds
+        
+        self.videoContainer.layer.addSublayer(playerLayer)
+    }
+    
+    func playVideo() {
+        self.player.play()
+    }
+    
+    func pauseVideo() {
+        self.player.pause()
+    }
 }
--- TokTik/Presentation/ViewControllers/VideoPostsTableViewController.swift
+++ TokTik/Presentation/ViewControllers/VideoPostsTableViewController.swift
@@ -12,40 +12,101 @@ class VideoPostsTableViewController: UITableViewController, UIMessageProtocol {
     
     var postController: PostModelController!
     
+    private var currentDisplayIndex = 0
+    
     private struct CellId {
         static let postCellId = "PostTableViewCell"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        self.addGestureRecognizers() 
         
         self.postController.getPosts(completion: { success in
             if success {
                 self.showMessage(title: "Nice", message: "everything works", type: .success)
+                self.tableView.reloadData()
             } else {
                 self.showMessage(title: "Oups", message: "Could not fetch posts", type: .error)
             }
         })
     }
+    
+    func addGestureRecognizers() {
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swipeUpGesture))
+        swipeUp.direction = UISwipeGestureRecognizer.Direction.up
+        self.view.addGestureRecognizer(swipeUp)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(swipeDownGesture))
+        swipeDown.direction = UISwipeGestureRecognizer.Direction.down
+        self.view.addGestureRecognizer(swipeDown)
+    }
+    
+    func displayNewCellAtIndex(_ newIndex: Int) {
+        self.currentDisplayIndex = newIndex
+        let newIndexPath = IndexPath(row: self.currentDisplayIndex, section: 0)
+        self.tableView.scrollToRow(at: newIndexPath, at: .top, animated: true)
+    }
+    
+    @IBAction func swipeUpGesture(_ sender: Any) {
+        let nextIndex = self.currentDisplayIndex + 1
+        if nextIndex < self.postController.postsCount() {
+            self.displayNewCellAtIndex(nextIndex)
+        }
+    }
+    
+    @IBAction func swipeDownGesture(_ sender: Any) {
+        let nextIndex = self.currentDisplayIndex - 1
+        if nextIndex >= 0 {
+            self.displayNewCellAtIndex(nextIndex)
+        }
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
 
-    // MARK: - Table view data source
+// MARK: - Table view data source
 
+extension VideoPostsTableViewController {
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return 0
+        return self.postController.postsCount()
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CellId.postCellId, for: indexPath) as? PostTableViewCell else { return UITableViewCell() }
         
-
+        let post = self.postController.post(at: indexPath.row)
+        cell.setup(title: post.title, videoUrl: post.videoURL)
+        
         return cell
     }
+}
+
+ // MARK: - Table view delegate
+
+extension VideoPostsTableViewController {
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.tableView.bounds.size.height
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let postCell = cell as? PostTableViewCell else { return }
+        postCell.playVideo()
+    }
+    
+    override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let postCell = cell as? PostTableViewCell else { return }
+        postCell.pauseVideo()
+    }
+
 }
